9b83f5ab1e606115bee29f204e6ea508d824d811
HUANGHO2 <HUANGHO2@OOCL.COM>
2024-11-19 11:47:15 +0800

    feat,new feature generateLine

--- src/main/java/com/tw/MultiplicationTableBuilder.java
+++ src/main/java/com/tw/MultiplicationTableBuilder.java
@@ -12,4 +12,21 @@ public class MultiplicationTableBuilder {
     public boolean isValid(int start, int end) {
         return isInRange(start)&&isInRange(end)&&isStarterNoBiggerthanEnd(start,end);
     }
+
+    public String generateLine(int start,int end){
+        //todo Stringbuilder
+        //todo format
+        StringBuilder testLine=new StringBuilder();
+        String line="";
+        for(int i=start;i<=end;i++){
+            int result=i*end;
+            if(i!=end){
+                String.format("%d*%d=%d",i,end,result);
+                line=line+Integer.toString(i)+'*'+ Integer.toString(end)+'='+ Integer.toString(result)+" ";
+            }else{
+                line=line+Integer.toString(i)+'*'+ Integer.toString(end)+'='+ Integer.toString(result);
+            }
+        }
+        return line;
+    }
 }
